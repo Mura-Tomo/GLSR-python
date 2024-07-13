c6e563d6614b438cb180834732c6d88d702d43ce
Murata <tomoya49m@gmail.com>
2024-07-14 00:15:51 +0900

    [fixed]

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import random
 
 app = Flask(__name__)
 
-@app.route('/api/random-number', methods=['GET'])
+@app.route('/')
 def random_number():
     number = random.randint(1, 100)
     return jsonify({'number': number})
