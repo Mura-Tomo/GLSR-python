6f2771fbeb40d0b6e6e02ee65f06d64e3f56c7eb
Murata <tomoya49m@gmail.com>
2024-07-14 00:19:28 +0900

    [fixed]

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
+import os
 from flask import Flask, jsonify
 import random
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/api/random-number', methods=['GET'])
 def random_number():
     number = random.randint(1, 100)
     return jsonify({'number': number})
 
 if __name__ == '__main__':
-    host = os.getenv('HOST', '0.0.0.0')  
-    port = int(os.getenv('PORT', 5000))   
+    host = os.getenv('HOST', '0.0.0.0')
+    port = int(os.getenv('PORT', 5000))
     app.run(host=host, port=port)
